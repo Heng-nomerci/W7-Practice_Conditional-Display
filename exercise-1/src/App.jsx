d40f3d56b353d92963738a72258a364d1fc2a93a
Heng <bunheng.try@student.cadt.edu.kh>
2025-03-08 22:04:41 +0700

    Done

---
+++ exercise-1/src/App.jsx
@@ -0,0 +1,25 @@
+import Header from "./components/Header";
+import Scores from "./components/Scores";
+import {
+  HTML_RESULTS,
+  JAVA_RESULTS,
+  PYTHON_RESULTS,
+  ENGLISH_RESULTS,
+} from "./data";
+
+function App() {
+  return (
+    <>
+      <Header batchName="Batch 2025" />
+
+      <main className="scores-container">
+        <Scores courseName="HTML" courseResults={HTML_RESULTS} />
+        <Scores courseName="JAVA" courseResults={JAVA_RESULTS} />
+        <Scores courseName="JAVA" courseResults={PYTHON_RESULTS} />
+        <Scores courseName="JAVA" courseResults={ENGLISH_RESULTS} />
+      </main>
+    </>
+  );
+}
+
+export default App;
